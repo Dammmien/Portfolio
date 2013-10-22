5e0d6d9c676a0c04e03a8418e46de1602fe1cfd5
Dammmien <d.damien@live.fr>
2013-10-22 22:09:17 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var express  = require('express');
+var jade     = require('jade');
+var fs       = require('fs');
+var less     = require('less');
+
+var app      = express();
+
+app.get('/', function(req, res) {
+  var html = jade.renderFile('views/home.jade');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(html);
+}).get('/:page', function(req, res) {
+  var html = jade.renderFile('views/' + req.params.page + '.jade', function (err, html) {
+    if (err) return jade.renderFile('views/404.jade');
+    else return html;
+  });
+  res.setHeader('Content-Type', 'text/html');
+  res.charset = 'utf-8';
+  res.end(html);
+}).get('/projects/:project', function(req, res) {
+  var html = jade.renderFile('views/projects/' + req.params.project + '.jade', function (err, html) {
+    if (err)return jade.renderFile('views/404.jade');
+    else return html;
+  });
+  res.setHeader('Content-Type', 'text/html');
+  res.charset = 'utf-8';
+  res.end(html);
+}).get('*.less', function(req,res){
+  var path = __dirname + req.url;
+  fs.readFile(path, "utf8", function(err, data) {
+    if (err) throw err;
+    less.render(data, function(err, css) {
+      if (err) throw err;
+      res.header("Content-type", "text/css");
+      res.send(css);
+    });
+  });
+}).use("/", express.static(__dirname));
+
+app.listen(3000);
