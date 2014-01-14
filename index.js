f46723a0fce3f9837b31e746a7a792282511c045
Dammmien <d.damien@live.fr>
2014-01-14 13:49:53 +0000

    update

--- index.js
+++ index.js
@@ -24,16 +24,6 @@ app.get('/', function(req, res) {
   res.setHeader('Content-Type', 'text/html');
   res.charset = 'utf-8';
   res.end(html);
-}).get('*.less', function(req,res){
-  var path = __dirname + req.url;
-  fs.readFile(path, "utf8", function(err, data) {
-    if (err) throw err;
-    less.render(data, function(err, css) {
-      if (err) throw err;
-      res.header("Content-type", "text/css");
-      res.send(css);
-    });
-  });
 }).use("/", express.static(__dirname));
 
 app.listen(process.env.PORT || 3000);
