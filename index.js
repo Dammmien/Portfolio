1461662b9687124e978278c5043c80f18202d289
Dammmien <d.damien@live.fr>
2014-01-16 12:07:57 +0000

    update

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/', function(req, res) {
   res.setHeader('Content-Type', 'text/html');
   res.charset = 'utf-8';
   res.end(html);
+}).get('/experiments/:experience', function(req, res) {
+  var html = jade.renderFile('views/experiments/' + req.params.experience + '.jade', function (err, html) {
+    if (err)return jade.renderFile('views/404.jade');
+    else return html;
+  });
+  res.setHeader('Content-Type', 'text/html');
+  res.charset = 'utf-8';
+  res.end(html);
 }).use("/", express.static(__dirname));
 
 app.listen(process.env.PORT || 3000);
